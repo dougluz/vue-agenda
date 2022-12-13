68f9e44a4b325704b4c88a2035349b08cf25fc54
dougluz <dougs.2210@gmail.com>
2022-12-13 15:40:11 -0300

    style: added new colors and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-red": "#fa7268",
+        "light-yellowish-green": "#dbff90",
+        "light-background": "#f8f9fd",
+        dark: "#2a2d3b",
+      },
+    },
   },
   plugins: [],
 };
