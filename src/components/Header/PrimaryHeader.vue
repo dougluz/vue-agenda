befd0a75dd5b2a7ccc00181ba20f3eb6f16aa3f4
dougluz <dougs.2210@gmail.com>
2022-12-13 16:22:25 -0300

    feat: added primary header component

---
+++ src/components/Header/PrimaryHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import UbookLogo from "@/components/icons/UbookLogo.vue";
+import { useContactStore } from "@/stores/useContactStore";
+import SearchInput from "../SearchInput/SearchInput.vue";
+import AddButton from "../Buttons/AddButton.vue";
+
+const contactStore = useContactStore();
+const isListEmpty = computed(() => contactStore.isEmpty());
+</script>
+
+<template>
+  <header class="w-full h-16">
+    <div class="w-full p-4 flex items-center justify-between">
+      <UbookLogo class="hidden md:inline-flex h-8 w-37" alt="ubook-logo" />
+      <div class="flex w-full items-center justify-end gap-6">
+        <AddButton small v-if="!isListEmpty" />
+        <SearchInput />
+      </div>
+    </div>
+  </header>
+</template>
