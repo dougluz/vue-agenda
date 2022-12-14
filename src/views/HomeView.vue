3de0c37ac7e3fb571719af87361f5d3a15a844db
dougluz <dougs.2210@gmail.com>
2022-12-14 14:49:22 -0300

    feat: update home page view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import EmptyListVue from "@/components/EmpytList/EmptyList.vue";
+import { useContactStore } from "@/stores/useContactStore";
+
+const contactStore = useContactStore();
+const isListEmpty = computed(() => contactStore.isEmpty());
+</script>
+
 <template>
   <main>
-    <p>hello</p>
+    <EmptyListVue v-if="isListEmpty" />
   </main>
 </template>
