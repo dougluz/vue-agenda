7961a0fc2cf6787b34caaa745152c3f840c19fc1
dougluz <dougs.2210@gmail.com>
2022-12-14 20:21:19 -0300

    style: added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ module.exports = {
     extend: {
       colors: {
         "light-red": "#fa7268",
+        "very-light-pink": "#fff3f2",
+        salmon: "#fa8d68",
         "light-yellowish-green": "#dbff90",
         "light-background": "#f8f9fd",
         dark: "#2a2d3b",
+        "blue-grey": "#9198af",
+        "light-blue-grey": "#e1e1e1",
       },
     },
   },
