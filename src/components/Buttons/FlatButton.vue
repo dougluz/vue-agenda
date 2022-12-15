e6bc0698c6fbae61cae6a8854b32133af981dec9
dougluz <dougs.2210@gmail.com>
2022-12-15 20:11:41 -0300

    feat: update buttons styles

--- src/components/Buttons/FlatButton.vue
+++ src/components/Buttons/FlatButton.vue
@@ -6,7 +6,7 @@
 
 <style scoped>
 .flat-button {
-  @apply text-sm font-medium text-light-red rounded;
+  @apply flex items-center h-8 px-4 py-2 text-sm font-medium text-light-red rounded-2xl;
   padding: 0.75rem 1.375rem 0.75rem 1rem;
 }
 </style>
--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -1,11 +1,11 @@
 <template>
-  <button class="bg-light-red h-8 pt-2 pb-2 pl-4 pr-4 rounded-2xl">
+  <button class="primary-button">
     <slot> Salvar </slot>
   </button>
 </template>
 
 <style scoped>
 .primary-button {
-  @apply bg-light-red text-white text-sm font-medium h-8 pt-2 pb-2 pl-4 pr-4 rounded-2xl;
+  @apply flex items-center bg-light-red text-white text-sm font-medium h-8 px-4 py-2 rounded-2xl;
 }
 </style>
