4381de95369f96b9f0a20a40b30d12be4e81c3d3
dougluz <dougs.2210@gmail.com>
2022-12-16 15:45:04 -0300

    feat: added contacts table

--- src/components/ContactsTable/ContactsTable.vue
+++ src/components/ContactsTable/ContactsTable.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import { useContactStore } from "@/stores/useContactStore";
 import ContactAvatar from "../ContactAvatar/ContactAvatar.vue";
 import IcEdit from "../icons/IcEdit.vue";
 import IcDelete from "../icons/IcDelete.vue";
+import { useModalStore } from "@/stores/useModalStore";
 
 const contactsStore = useContactStore();
+const modalStore = useModalStore();
 
-const contacts = computed(() => contactsStore.getAllContacts());
+const handleDelete = (email: string) => {
+  contactsStore.setSelectedContactEmail(email);
+  modalStore.setModalStatus("deleteModal", true);
+};
+
+// write a function to format a phone number in the format (xx) xxxxx-xxxx
+const formatPhoneNumber = (phoneNumber: string) => {
+  const phone = phoneNumber.replace(/\D/g, "");
+  const phoneLength = phone.length;
+
+  if (phoneLength < 11) {
+    return phone.replace(/(\d{2})(\d{4,5})(\d{4})/, "($1) $2-$3");
+  }
+
+  return phone.replace(/(\d{2})(\d{1})(\d{4,5})(\d{4})/, "($1) $2 $3-$4");
+};
 </script>
 
 <template>
@@ -27,18 +43,18 @@ const contacts = computed(() => contactsStore.getAllContacts());
       <tbody>
         <tr
           class="h-10 bg-white px-2 py-4 hover:bg-very-light-pink"
-          v-for="contact in contacts"
-          :key="contact.id"
+          v-for="contact in contactsStore.contacts"
+          :key="contact.email"
         >
           <td class="pl-2 py-4 w-10">
             <ContactAvatar :contactName="contact?.name" />
           </td>
           <td class="py-4">{{ contact?.name }}</td>
           <td class="py-4">{{ contact?.email }}</td>
-          <td class="py-4">{{ contact?.phone }}</td>
+          <td class="py-4">{{ formatPhoneNumber(contact?.phone) }}</td>
           <td class="flex items-center justify-end gap-6 pr-2 py-4">
             <button><IcEdit /></button>
-            <button><IcDelete /></button>
+            <button @click="handleDelete(contact.email!)"><IcDelete /></button>
           </td>
         </tr>
       </tbody>
