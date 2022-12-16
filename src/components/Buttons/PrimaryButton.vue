634825e2992b1164df6ecb25e3ada51c95ab27ab
dougluz <dougs.2210@gmail.com>
2022-12-16 15:41:18 -0300

    feat: updated primary button style

--- src/components/Buttons/PrimaryButton.vue
+++ src/components/Buttons/PrimaryButton.vue
@@ -6,6 +6,6 @@
 
 <style scoped>
 .primary-button {
-  @apply flex items-center bg-light-red text-white text-sm font-medium h-8 px-4 py-2 rounded-2xl;
+  @apply flex items-center bg-light-red text-white text-sm font-medium h-8 px-4 py-2 rounded-2xl disabled:bg-opacity-30 disabled:cursor-not-allowed disabled:text-opacity-90;
 }
 </style>
